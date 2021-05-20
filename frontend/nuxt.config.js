b208d9062560eb589bed73292c1de396487be8f8
Atsocs <atsocs.cc@gmail.com>
2021-05-20 14:14:10 -0300

    change page title from "frontend" to "avalita"

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -6,8 +6,8 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - frontend',
-    title: 'frontend',
+    titleTemplate: '%s - avalITA',
+    title: 'avalITA',
     htmlAttrs: {
       lang: 'en'
     },
