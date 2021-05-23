9bc2586be3bb53b14953ee7536d7379d4f9ba8e4
gbrlbrbs <gabrielbarbosamartinz@gmail.com>
2021-05-23 15:12:08 -0300

    area do aluno - initial

---
+++ frontend/pages/area-aluno.vue
@@ -0,0 +1,19 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "area-aluno",
+  head () {
+    const title = "Área do Aluno"
+    return {
+      title
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
