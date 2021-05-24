66eb3320cf2253457c76c625ba7482626ab91cd8
Marina <m.goncalvesm2018@gmail.com>
2021-05-23 23:27:16 -0300

    adding files of area do aluno

--- frontend/pages/avaliacao.vue
+++ frontend/pages/avaliacao.vue
@@ -4,25 +4,24 @@
     <v-card-title class="headline">
       Área do aluno
     </v-card-title>
+
     <v-btn
       color="primary"
       nuxt
-      to="/avaliacao"
+      to="/comentario"
     >
-      + adicione sua avaliação
+      + adicione seu comentário
     </v-btn>
+
     <v-row dense align="stretch">
       <!--      1ºbloco-->
-      <v-col cols="4">
+      <v-col cols="6">
         <v-card>
           <v-card-text class="primary--text">
-            <v-btn
-              color="primary"
-              text
-              @click="reserve"
-            >
-              CES 111 - PROFESSORA MARINA
-            </v-btn>
+            <h5 class="text-truncate text-uppercase">CES777 - PROFESSOR ESPONGEBOB</h5>
+            <!--            <h1>{{ sales }}</h1>-->
+            <p>Pontualidade</p>
+
             <v-rating
               align = "center"
               :value="4.5"
@@ -34,13 +33,7 @@
             ></v-rating>
             <div align = "center"  class="grey--text ms-4">4.5</div>
 
-            <v-btn
-              color="primary"
-              text
-              @click="reserve"
-            >
-              CES 000 - PROFESSORA MARINA
-            </v-btn>
+            <p>Coerência da cobrança</p>
             <v-rating
               align = "center"
               :value="3.5"
@@ -52,13 +45,7 @@
             ></v-rating>
             <div align = "center"  class="grey--text ms-4">4.5</div>
 
-            <v-btn
-              color="primary"
-              text
-              @click="reserve"
-            >
-              CES 555 - PROFESSORA MARINA
-            </v-btn>
+            <p>Possibilidade de melação</p>
             <v-rating
               align = "center"
               :value="2.0"
@@ -72,8 +59,8 @@
           </v-card-text>
         </v-card>
       </v-col>
-      </v-card-text>
-      </v-card>
+          </v-card-text>
+        </v-card>
       </v-col>
     </v-row>
   </v-container>
@@ -81,16 +68,11 @@
 
 <script>
 export default {
-  name: "area-aluno",
+  name: "avaliacao",
   head() {
-    const title = "Área do Aluno"
+    const title = "Avaliação"
     return {
       title
-    }
-  },
-  data() {
-    return {
-      variavel: 14
     }
   },
 }
