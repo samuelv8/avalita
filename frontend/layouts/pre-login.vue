fee95e26022c233963d72c804d53e588690af2e6
Atsocs <atsocs.cc@gmail.com>
2021-05-24 20:07:45 -0300

    botões de login e registrar na barra superior

---
+++ frontend/layouts/pre-login.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      min-height="80"
+    >
+      <v-img
+        max-height="200"
+        max-width="240"
+        src="https://user-images.githubusercontent.com/24798860/119005862-3318d400-b966-11eb-865a-6c22fb983c7c.png"
+      ></v-img>
+      <v-spacer/>
+      <v-btn>Registrar</v-btn>
+      <v-btn>Login</v-btn>
+    </v-app-bar>
+    <v-spacer/>
+    <v-main>
+      <v-container>
+        <Nuxt/>
+      </v-container>
+    </v-main>
+    <v-footer
+      :absolute="!fixed"
+      app
+    >
+      <span>{{ title }} &copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      fixed: false,
+      title: 'avalITA',
+    }
+  }
+}
+</script>
---
+++ frontend/pages/registrar.vue
@@ -0,0 +1,19 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "registre",
+  head() {
+    const title = "Registre"
+    return {
+      title
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
