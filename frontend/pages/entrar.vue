c37074dea2ffd15cdbfdd47ab329fe728d41e61b
Atsocs <atsocs.cc@gmail.com>
2021-05-25 09:03:55 -0300

    entrar/registrar

--- frontend/pages/entrar.vue
+++ frontend/pages/entrar.vue
@@ -1,9 +1,43 @@
 <template>
+  <v-form
+    ref="form"
+    v-model="valid"
+    lazy-validation
+  >
+    <v-text-field
+      v-model="email"
+      label="Email"
+      required
+    ></v-text-field>
 
+    <v-text-field
+      v-model="password"
+      label="Password"
+      required
+      visibility_off
+    ></v-text-field>
+
+    <v-btn
+      color="primary"
+      class="mr-4"
+      @click="printar_email"
+    >
+      Entrar
+    </v-btn>
+  </v-form>
 </template>
 
 <script>
 export default {
+  data: () => ({
+    email: '',
+    password: '',
+  }),
+  methods: {
+    printar_email () {
+      console.log('email is: ' + this.email)
+    },
+  },
   name: "entrar",
   head() {
     const title = "Entrar"
