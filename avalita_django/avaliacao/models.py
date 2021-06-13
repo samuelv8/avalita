bc8c47947a8bd41bc11bd544586cd501bdf04829
Atsocs <atsocs.cc@gmail.com>
2021-06-13 14:44:58 -0300

    criar modelo para Departamento

--- avalita_django/avaliacao/models.py
+++ avalita_django/avaliacao/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Departamento(models.Model):
+    nome = models.CharField(max_length=80)
+    slug = models.SlugField()
+
+    class Meta:
+        ordering = ('nome',)
+
+    def __str__(self):
+        return self.nome
+
+    def get_absolute_url(self):
+        return f'/dpto/{self.slug}/'
