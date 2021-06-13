7505ee3e22e41783db1853342e847a8b400dc104
Atsocs <atsocs.cc@gmail.com>
2021-06-13 14:55:41 -0300

    adiciona modelo para Disciplina

--- avalita_django/avaliacao/models.py
+++ avalita_django/avaliacao/models.py
@@ -1,15 +1,67 @@
+from io import BytesIO
+from PIL import Image
+
+from django.core.files import File
 from django.db import models
 
 
 class Departamento(models.Model):
-    nome = models.CharField(max_length=80)
+    name = models.CharField(max_length=80)
     slug = models.SlugField()
 
     class Meta:
-        ordering = ('nome',)
+        ordering = ('name',)
 
     def __str__(self):
-        return self.nome
+        return self.name
 
     def get_absolute_url(self):
         return f'/dpto/{self.slug}/'
+
+
+class Disciplina(models.Model):
+    departamento = models.ForeignKey(
+        Departamento, related_name='disciplinas', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='uploads/', blank=True, null=True)
+    thumbnail = models.ImageField(upload_to='uploads/', blank=True, null=True)
+    slug = models.SlugField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('-date_added',)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return f'/dpto/{self.departamento.slug}/{self.slug}'
+
+    def get_image(self):
+        if self.image:
+            return "http://127.0.0.1:8000" + self.image.url
+        return ''
+
+    def get_thumbnail(self):
+        if self.thumbnail:
+            return "http://127.0.0.1:8000" + self.thumbnail.url
+        else:
+            if self.image:
+                self.thumbnail = self.make_thumbnail(self.image)
+                self.save()
+
+                return "http://127.0.0.1:8000" + self.thumbnail.url
+            else:
+                return ''
+
+    def make_thumbnail(self, image, size=(300, 200)):
+        img = Image.open(image)
+        img.convert('RGB')
+        img.thumbnail(size)
+
+        thumb_io = BytesIO()
+        img.save(thumb_io, 'JPEG', quality=85)
+
+        thumbnail = File(thumb_io, name=image.name)
+
+        return thumbnail
