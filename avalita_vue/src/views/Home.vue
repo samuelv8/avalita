cf26541121a3477f3d7686b2836d12b8d23dc047
Atsocs <atsocs.cc@gmail.com>
2021-06-13 15:22:48 -0300

    home

--- avalita_vue/src/views/Home.vue
+++ avalita_vue/src/views/Home.vue
@@ -1,18 +1,40 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <section class="hero is-medium is-dark mb-6">
+      <div class="hero-body has-text-centered">
+        <p class="title mb-6">Bem-vindo ao Avalita</p>
+        <p class="subtitle">Eletivas avaliadas por iteanos</p>
+      </div>
+    </section>
+
+    <div class="columns is-multiline">
+      <div class="column is-12">
+        <h2 class="is-size-2 has-text-centered">Latest Disciplinas</h2>
+      </div>
+
+      <div
+        class="column is-3"
+        v-for="product in latestDisciplinas"
+        v-bind:key="product.id"
+      >
+        <div class="box">
+          <figure class="image mb-4">
+            <img v-bind:src="product.get_thumbnail" />
+          </figure>
+
+          <h3 class="is-size-4">{{ product.name }}</h3>
+          <p class="is-size-6 has-text-grey">${{ product.price }}</p>
+
+          View details
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  components: {},
+};
 </script>
