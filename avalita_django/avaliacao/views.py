26e14d1cd88b861cad1c1e58b83ab69a28ef7902
Atsocs <atsocs.cc@gmail.com>
2021-06-13 15:03:13 -0300

    criar visualização

--- avalita_django/avaliacao/views.py
+++ avalita_django/avaliacao/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from avalita_django.avaliacao.serializers import DisciplinaSerializer
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from .models import Disciplina
+
+
+class LatestDisciplinasList(APIView):
+    def get(self, request, format=None):
+        disciplinas = Disciplina.objects.all()[0:4]
+        serializer = DisciplinaSerializer(disciplinas, many=True)
+        return Response(serializer.data)
